99afc82353e752a3796b96f6487a5688a051ba6c
Mohamed8Aithassoun <mohamed.ait-hassoun@edu.uiz.ac.ma>
2024-01-18 18:38:39 +0100

    ha man zoydh

--- pages/urls.py
+++ pages/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('offers',views.offers_page, name='offers_page'),
     path('login_validation',views.login_validation, name='login_validation'),
     path('signup_validation',views.signup_validation, name='signup_validation'),
+    path('Code_validation',views.get_verificaioncode, name='Code_validation'),
+    path('signup_validation1',views.email_validation, name='signup_validation1'),
+    path('testmail',views.simple_mail),
 ]
