bab977f049da610ff8f2dcd3403798a9363ec8a9
Ounaceur <abdeljalil.ounaceur2003@gmail.com>
2024-01-19 18:55:22 +0100

    made the offer model not automatically deleted after a category deletion

--- pages/models.py
+++ pages/models.py
@@ -55,8 +55,10 @@ class Offre(models.Model):
     image = models.ImageField(upload_to='images/', null=True)
     date_debut = models.DateTimeField()
     date_fin = models.DateTimeField()
-    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    categorie = models.ForeignKey(Categorie, on_delete=models.SET_NULL, null=True)
     promotion = models.ForeignKey(Promotion, on_delete=models.SET_NULL, null=True)
+    is_active = models.BooleanField(default=True)
+
 
 class Reservation(models.Model):
     utilisateur = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
